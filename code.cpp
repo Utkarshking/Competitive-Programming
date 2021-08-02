c8a64a7a0d0f76103c139fb2dea6855aaee0e294
Utkarshking <61401484+Utkarshking@users.noreply.github.com>
2021-08-02 13:12:04 +0530

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define ll 			long long int
+#define si(x)	    scanf("%d",&x)
+#define sl(x)		scanf("%lld",&x)
+#define ss(x)		scanf("%s",s)
+#define pi(x)		printf("%d\n",x)
+#define pl(x)		printf("%lld\n",x)
+#define ps(s)		printf("%s\n",s)
+#define deb1(x)    	cout<<#x<<"="<<x
+#define deb2(x,y)	cout<<#x<<"="<<x<<","<<#y<<"="<<y<<"\n"
+#define pb			emplace_back
+#define F 			first
+#define S			second
+#define sort(x)		sort(all(x))
+#define pii			pair<int,int>
+#define pll			pair<ll,ll>
+#define vec			vector<int,int>
+#define PI          3.1415926535897932384626
+using namespace std;
+void solve(ll n) {
+	deb1(n);
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	si(t);
+	while (t--) {
+		int n;
+		// cin >> n;
+		si(n);
+		solve(n);
+
+	}
+}
